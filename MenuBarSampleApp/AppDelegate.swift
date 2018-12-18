7a6904f522fbcd6e8def35eec09f36041b061496
isaoeka <isaoeka@gmail.com>
2018-12-18 14:22:56 +0900

    :sparkles: Add xcodeproj

---
+++ MenuBarSampleApp/AppDelegate.swift
@@ -0,0 +1,23 @@
+//
+//  AppDelegate.swift
+//  MenuBarSampleApp
+//
+//  Created by isaoeka on 2018/12/17.
+//  Copyright © 2018 isaoeka. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+    
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+}
+
---
+++ MenuBarSampleApp/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  MenuBarSampleApp
+//
+//  Created by isaoeka on 2018/12/17.
+//  Copyright © 2018 isaoeka. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
